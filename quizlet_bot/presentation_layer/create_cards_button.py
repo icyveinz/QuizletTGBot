60c66cecc37e0056dcde5ef33e8a6850023afc27
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-10 21:16:45 +0300

    //

--- quizlet_bot/presentation_layer/create_cards_button.py
+++ quizlet_bot/presentation_layer/create_cards_button.py
@@ -9,20 +9,6 @@ from ui_layer.create_cards_keyboards import CreateCardsKeyboards
 router = Router()
 
 
-# @router.message(F.text == "Create Cards")
-# async def handle_create_cards_button(message: Message, db: AsyncSession):
-#     user_id = str(message.from_user.id)
-#     user_service = UserService(db)
-#     success = await user_service.update_user_state(
-#         user_id, StatesEnum.AWAITING_FRONT.value
-#     )
-#
-#     if success:
-#         await message.reply("Please enter the front side of the card.")
-#     else:
-#         await message.reply("An error occurred. Please try again later.")
-
-
 @router.message(F.text == "Create Cards")
 async def handle_create_cards_button(message: Message, db: AsyncSession):
     user_id = str(message.from_user.id)
@@ -38,9 +24,17 @@ async def handle_create_cards_button(message: Message, db: AsyncSession):
 
 @router.message(F.text == "Добавить вручную", UserStateFilter(StatesEnum.CREATING_CARDS.value))
 async def handle_add_manual_button(message: Message, db: AsyncSession):
-    await message.reply(text="Выбран мануальный режим для загрузки")
+    user_id = str(message.from_user.id)
+    user_service = UserService(db)
+    success = await user_service.update_user_state(
+        user_id, StatesEnum.AWAITING_FRONT.value
+    )
+    if success:
+        await message.reply("Введите лицевую сторону карты")
+    else:
+        await message.reply("Произошла ошибка, попробуйте позже")
 
 
 @router.message(F.text == "Загрузить пакетом", UserStateFilter(StatesEnum.CREATING_CARDS.value))
 async def handle_add_auto_button(message: Message, db: AsyncSession):
-    await message.reply(text="Выбран автоматический режим для загрузки")
+    await message.reply(text="Выбран автоматический режим для загрузки\nЗагрузите пары разделенные символом ")
