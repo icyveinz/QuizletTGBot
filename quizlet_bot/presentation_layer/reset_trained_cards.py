f300a2634b3fb2ec660defc48cb6aec18de39773
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-12 00:36:31 +0300

    //

--- quizlet_bot/presentation_layer/reset_trained_cards.py
+++ quizlet_bot/presentation_layer/reset_trained_cards.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from service_layer.card_service import CardService
+from ui_layer.start_command_keyboards import StartCommandKeyboards
 
 router = Router()
 
@@ -17,7 +18,11 @@ async def reset_trained_cards_handler(message: Message, db: AsyncSession):
 
     if reset_count > 0:
         await message.reply(
-            f"{reset_count} cards were reset so you can review them again."
+            f"{reset_count} cards were reset so you can review them again.",
+            reply_markup=StartCommandKeyboards.startup_card_builder()
         )
     else:
-        await message.reply("No studied cards found to reset.")
+        await message.reply(
+            "No studied cards found to reset.",
+                reply_markup=StartCommandKeyboards.startup_card_builder()
+        )
--- quizlet_bot/presentation_layer/view_cards_button.py
+++ quizlet_bot/presentation_layer/view_cards_button.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 from service_layer.card_service import CardService
+from ui_layer.start_command_keyboards import StartCommandKeyboards
 from utilities.message_chunker import chunk_message
 
 router = Router()
@@ -19,9 +20,13 @@ async def handle_view_cards_button(message: Message, db: AsyncSession):
         for index, card in enumerate(user_cards):
             response += f"{index + 1})\n<b>Аверс:</b> {card.front_side}\n<i>Реверс:</i> {card.back_side}\n\n"
         message_chunks = chunk_message(response)
-        for chunk in message_chunks:
-            await message.answer(chunk)
+        for i, chunk in enumerate(message_chunks):
+            if i == len(message_chunks) - 1:
+                await message.answer(chunk, reply_markup=StartCommandKeyboards.startup_card_builder())
+            else:
+                await message.answer(chunk)
     else:
         await message.answer(
-            "You don't have any cards yet. Use the 'Create Cards' button to add new cards."
+            "You don't have any cards yet. Use the 'Create Cards' button to add new cards.",
+            reply_markup=StartCommandKeyboards.startup_card_builder()
         )
